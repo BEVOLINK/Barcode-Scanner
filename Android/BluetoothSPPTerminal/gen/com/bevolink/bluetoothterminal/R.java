e2cb08889e37747db96d1f790368f70e5e28755f
Alan.Long <pc128@163.com>
2014-09-22 23:36:26 +0800

    Fix build issues

--- Android/BluetoothSPPTerminal/gen/com/bevolink/bluetoothterminal/R.java
+++ Android/BluetoothSPPTerminal/gen/com/bevolink/bluetoothterminal/R.java
@@ -10,39 +10,52 @@ package com.bevolink.bluetoothterminal;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        public static final int bluetooth_spp_padding=0x7f040001;
+        public static final int bluetooth_spp_text_size=0x7f040000;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int layout1=0x7f070002;
-        public static final int menu_device_connect=0x7f070006;
-        public static final int menu_manual_input=0x7f070007;
-        public static final int menu_submit_data=0x7f070008;
-        public static final int scrollView1=0x7f070001;
-        public static final int textRead=0x7f070003;
-        public static final int textStatus=0x7f070000;
-        public static final int txtLabelTotalTicket=0x7f070004;
-        public static final int txtTotalTicket=0x7f070005;
+        public static final int button_scan=0x7f080001;
+        public static final int layout1=0x7f080004;
+        public static final int list_devices=0x7f080000;
+        public static final int menu_device_connect=0x7f080008;
+        public static final int menu_manual_input=0x7f080009;
+        public static final int menu_submit_data=0x7f08000a;
+        public static final int scrollView1=0x7f080003;
+        public static final int textRead=0x7f080005;
+        public static final int textStatus=0x7f080002;
+        public static final int txtLabelTotalTicket=0x7f080006;
+        public static final int txtTotalTicket=0x7f080007;
     }
     public static final class layout {
-        public static final int layout_main=0x7f030000;
+        public static final int device_list=0x7f030000;
+        public static final int device_name=0x7f030001;
+        public static final int layout_main=0x7f030002;
     }
     public static final class menu {
-        public static final int connection=0x7f060000;
-        public static final int disconnection=0x7f060001;
+        public static final int connection=0x7f070000;
+        public static final int disconnection=0x7f070001;
     }
     public static final class string {
-        public static final int action_settings=0x7f040001;
-        public static final int app_name=0x7f040000;
-        public static final int label_connected=0x7f040006;
-        public static final int label_failed_connection=0x7f040005;
-        public static final int label_no_bluetooth=0x7f040004;
-        public static final int label_no_connection=0x7f040002;
-        public static final int label_total_ticket=0x7f040003;
-        public static final int menu_device_connect=0x7f040007;
-        public static final int menu_disconnect=0x7f04000a;
-        public static final int menu_manual_input=0x7f040008;
-        public static final int menu_submit_data=0x7f040009;
+        public static final int action_settings=0x7f050006;
+        public static final int app_name=0x7f050005;
+        public static final int label_bluetooth_devices=0x7f050004;
+        public static final int label_connected=0x7f05000b;
+        public static final int label_failed_connection=0x7f05000a;
+        public static final int label_no_bluetooth=0x7f050009;
+        public static final int label_no_connection=0x7f050007;
+        public static final int label_no_devices_found=0x7f050000;
+        public static final int label_scan_for_devices=0x7f050003;
+        public static final int label_scanning_for_devices=0x7f050001;
+        public static final int label_select_device_to_connect=0x7f050002;
+        public static final int label_total_ticket=0x7f050008;
+        public static final int menu_device_connect=0x7f05000c;
+        public static final int menu_disconnect=0x7f05000f;
+        public static final int menu_manual_input=0x7f05000d;
+        public static final int menu_submit_data=0x7f05000e;
     }
     public static final class style {
         /** 
@@ -66,10 +79,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
